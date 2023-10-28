8d5bd97611b0a7643e82b7246f65ec505c387b3b
wongam <wongam@kyonggi.ac.kr>
2023-10-28 17:49:23 +0900

    test : findAll

--- src/test/java/hello/servlet/domain/member/MemberRespositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRespositoryTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 class MemberRespositoryTest {
 
     MemberRepository memberRepository = MemberRepository.getInstance(); // 테스트 할 대상
@@ -22,6 +26,23 @@ class MemberRespositoryTest {
         Member savedMember = memberRepository.save(member);
         //then
         Member findMember = memberRepository.findById(savedMember.getId());
-        Assertions.assertThat(findMember).isEqualTo(savedMember);
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll(){
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
     }
 }
