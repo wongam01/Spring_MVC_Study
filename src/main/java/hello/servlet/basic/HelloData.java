5cd33c3bb4c11e261f31b5be53a43df3141e98c9
wongam <wongam@kyonggi.ac.kr>
2024-04-11 14:26:44 +0900

    study : Http-Request-Data

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class HelloData {
+    private String username;
+    private int age;
+
+
+}
