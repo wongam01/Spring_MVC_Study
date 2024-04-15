8b9ce44b8ca0054ae8051f63c72ca9355a9ba855
wongam <wongam@kyonggi.ac.kr>
2024-04-15 15:47:20 +0900

    Study : Introduction of FrontController

--- src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -1,5 +1,13 @@
 package hello.servlet.web.frontcontroller.v1;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 public interface ControllerV1 {
+    // Servlet 과 비슷한 모양의 컨트롤러 인터페이스를 도입한다.
+    void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 
 }
---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath); // 컨트롤러 -> 뷰로 이동
+        dispatcher.forward(request, response); // servlet 에서 JSP 호출 (서버 내부)
+
+    }
+}
